e5316c3c4f00117a2ad9c065a5496f7ff488b19b
Daniel Danuega <godaniel135@gmail.com>
2020-09-11 17:52:13 +0700

    feat: fix

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Mask Detector with TFLite Object Detection API in Microcontroller
+
+This project using python 3.6
+
 ## Raspberry Pi 4
+
 1. `cd mask-detector`
 2. `workon mask`
 3. Comment the opencv-python in requirements.txt
@@ -7,10 +11,12 @@
 5. `python TFLite_detection_webcam.py --modeldir=TFLite_model`
 
 ## In Desktop
+
 1. `cd mask-detector`
 2. activate venv
 3. `pip install requirements.txt`
 4. `python TFLite_detection_webcam.py --modeldir=TFLite_model`
 
 ## Cite
-TFLite_model is made by https://github.com/wangyifan411/Face-Mask-Type-Detector 
+
+TFLite_model is made by https://github.com/wangyifan411/Face-Mask-Type-Detector
--- quantization.py
+++ quantization.py
@@ -2,52 +2,74 @@ import numpy as np
 import tensorflow as tf
 from imutils import paths
 import cv2
+from subprocess import PIPE, Popen
 
 """
 This is the quantization script for tflite model float32 to int8 model.
-I use the original dataset by Yifan Wang for calibration purpose https://drive.google.com/drive/folders/1w-zoOuSauw5xBhOrfZpjcglvDH14mlGu.
+I use the original dataset by Wang Yifan for calibration purpose https://drive.google.com/drive/folders/1w-zoOuSauw5xBhOrfZpjcglvDH14mlGu.
 
-The dataset calibration is not included in this repo, please see the issue pages of Yifan Wang for further information. 
+The dataset calibration is not included in this repo, please see the issue pages of Wang Yifan for further information. 
 https://github.com/wangyifan411/Face-Mask-Type-Detector/issues/1
 
 FYI:
-the model that is used by Yifan Wang is SSD_Mobilenet_V2_COCO, where the size is 300x300.
+the model that is used by Wang Yifan is SSD_Mobilenet_V2_COCO, where the size is 300x300.
 You could find more information about size, batch_size, etc, of the pre-trained model in tensorflow object detection model zoo 1.
 This repo use Tensorflow 1
 """
 
-SIZE = (300, 300)
-BATCH_SIZE = 24
+# normH, normW = (300, 300)
+# BATCH_SIZE = 24
+
+script = [
+    "tflite_convert",
+    "--output_file=./TFLite_model/detect_model_quant.tflite",
+    "--graph_def_file=./TFLite_model/tflite_graph.pb",
+    "--input_format=TENSORFLOW_GRAPHDEF",
+    "--output_format=TFLITE",
+    "--input_shapes=1,300,300,3",
+    "--input_arrays=normalized_input_image_tensor",
+    "--output_arrays=TFLite_Detection_PostProcess,TFLite_Detection_PostProcess:1,TFLite_Detection_PostProcess:2,TFLite_Detection_PostProcess:3",
+    "--inference_type=QUANTIZED_UINT8",
+    "--std_dev_values=128",
+    "--mean_values=128",
+    "--allow_custom_ops",
+]
+
+output = Popen(script, stdout=PIPE, stderr=PIPE)
+stdout, stderr = output.communicate()
+print(stdout)
+print(stderr)
 
 
-def representative_dataset_gen():
-    ds = []
-    for imgPath in paths.list_images("./dataset_quantize"):
-        img = cv2.imread(imgPath)
-        img = cv2.resize(img, SIZE)
-        img = img / 255.0
-        img = img.astype(np.float32)
-        ds.append(img)
-    ds = np.array(ds)
+# def representative_dataset_gen():
+#     ds = []
+#     for imgPath in paths.list_images("./dataset_quantize"):
+#         img = cv2.imread(imgPath)
+#         img = cv2.resize(img, (normH, normW))
+#         img = img / 128.0
+#         img = img.astype(np.float32)
+#         ds.append(img)
+#     ds = np.array(ds)
+#     print(f"[INFO] representative shape {ds.shape}")
 
-    images = tf.data.Dataset.from_tensor_slices(ds).batch(1)
+#     images = tf.data.Dataset.from_tensor_slices(ds).batch(1)
 
-    for image in images.take(BATCH_SIZE):
-        yield [image]
+#     for image in images.take(BATCH_SIZE):
+#         yield [image]
 
 
-# Convert
-converter = tf.lite.TFLiteConverter.from_saved_model(
-    "./output_inference_graph_mobile.pb/saved_model"
-)
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
+# # Convert
+# converter = tf.lite.TFLiteConverter.from_frozen_graph("TFLite_model/tflite_graph.pb")
+# converter.optimizations = [tf.lite.Optimize.DEFAULT]
 
-converter.representative_dataset = representative_dataset_gen
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-converter.inference_input_type = tf.int8
-converter.inference_output_type = tf.int8
-tflite_quant_model = converter.convert()
+# converter.representative_dataset = representative_dataset_gen
+# converter.target_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+# converter.inference_input_type = [tf.uint8]
+# converter.inference_output_type = [tf.uint8]
+# tflite_quant_model = converter.convert()
+# print("[INFO] Successfully convert")
 
-# Save
-model_dir = "./TFLite_model/detect_model_quant.tflite"
-model_dir.write_bytes(tflite_quant_model)
+# # Save
+# model_dir = "./TFLite_model/detect_model_quant.tflite"
+# model_dir.write_bytes(tflite_quant_model)
+# print("[INFO] Successfully saved")
